340ca241dc51ed5c1c8c87ad45b5bdef14f85dbb
soumajit23 <soumajit12b45@gmail.com>
2024-06-21 19:25:07 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -11,11 +11,11 @@ const server = net.createServer((socket) => {
 
     socket.on("data", (data) => {
         const request = data.toString();
-        if (request.startsWith('GET /')) {
+        if (request.startsWith('GET / ')) {
             const response = "HTTP/1.1 200 OK\r\n\r\n";
             socket.write(response);
         } else {
-            const response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            constresponse = "HTTP/1.1 404 Not Found\r\n\r\n";
             socket.write(response);
         }
         socket.end();
