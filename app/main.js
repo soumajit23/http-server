a7d5e2502198d8d36fec9a82760a526f849d8897
soumajit23 <soumajit12b45@gmail.com>
2024-06-21 19:21:11 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -5,10 +5,21 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.on("close", () => {
-    socket.end();
-  });
+    socket.on("close", () => {
+        socket.end();
+    });
+
+    socket.on("data", (data) => {
+        const request = data.toString();
+        if (request.startsWith('GET /')) {
+            const response = "HTTP/1.1 200 OK\r\n\r\n";
+            socket.write(response);
+        } else {
+            response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            socket.write(response);
+        }
+        socket.end();
+    });
 });
 
 server.listen(4221, "localhost");
