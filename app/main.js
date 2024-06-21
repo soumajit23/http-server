d78f3770338a3c6b7b0fe638472f104bda96c890
soumajit23 <soumajit12b45@gmail.com>
2024-06-22 00:50:44 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -15,7 +15,7 @@ const server = net.createServer((socket) => {
             const res = "HTTP/1.1 200 OK\r\n\r\n";
             socket.write(res);
         } else if (request.includes('/echo/')) {
-            str = request.split('/echo/');
+            str = request.split('/echo/')[1];
             length = str.length;
             const res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${length}\r\n\r\n${str}`;
             socket.write(res);
